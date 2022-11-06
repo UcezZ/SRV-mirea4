10a401999fa4f8f77c92545aa1626ae7ef9b352e
UcezZ <ucezz@mail.ru>
2022-11-07 02:00:20 +0300

    lab5

--- lab5/webroot/private/docs/form-upload.php
+++ lab5/webroot/private/docs/form-upload.php
@@ -27,8 +27,8 @@
             if (n.value.length == 0) {
                 n.value = f.value.replace(/^.*[\\\/]/, '');
                 n.value = n.value.substr(0, n.value.length - n.value.split('.').pop().length - 1);
+                n.value = n.value.substr(0, 64);
             }
-            n.value = n.value.substr(0, 64);
         };
     f.addEventListener('change', e);
     n.addEventListener('keypress', e);
