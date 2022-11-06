e983e71657c3406d563109e0cdc515bdc0dad3dd
UcezZ <ucezz@mail.ru>
2022-11-07 01:59:08 +0300

    lab5

--- lab5/webroot/private/docs/form-upload.php
+++ lab5/webroot/private/docs/form-upload.php
@@ -22,13 +22,14 @@
 <script>
     let
         f = document.getElementById('file'),
-        n = document.getElementById('name');
-
-    f.addEventListener('change', () => {
-        if (n.value.length == 0) {
-            n.value = f.value.replace(/^.*[\\\/]/, '');
-            n.value = n.value.substr(0, n.value.length - n.value.split('.').pop().length - 1);
-        }
-        n.value = n.value.substr(0, 64);
-    });
+        n = document.getElementById('name'),
+        e = function() {
+            if (n.value.length == 0) {
+                n.value = f.value.replace(/^.*[\\\/]/, '');
+                n.value = n.value.substr(0, n.value.length - n.value.split('.').pop().length - 1);
+            }
+            n.value = n.value.substr(0, 64);
+        };
+    f.addEventListener('change', e);
+    n.addEventListener('keypress', e);
 </script>
